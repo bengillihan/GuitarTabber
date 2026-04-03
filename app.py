ac58594fdc4079b5ef9c1c4e01288a0b5abd2bf8
bengillihan <bdgillihan@gmail.com>
2026-04-03 10:23:24 -0700

    configure app for production with gunicorn and environment-based port settings

--- app.py
+++ app.py
@@ -1,3 +1,4 @@
+import os
 from pathlib import Path
 from typing import Optional
 
@@ -339,4 +340,5 @@ def index():
 
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    port = int(os.getenv("PORT", "8080"))
+    app.run(host="0.0.0.0", port=port, debug=False)
